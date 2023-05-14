d60974dc4425431e3b41bcf0210cdf1ddc173c30
FU3X <97900500+FU3X@users.noreply.github.com>
2023-05-13 22:13:21 -0500

    updated ui

--- src/lib/Verse.svelte
+++ src/lib/Verse.svelte
@@ -6,9 +6,28 @@
     async function generate_context() {
         generate_contextMsg = await invoke('generate_context')
     }
-  </script>
+</script>
   
-  <div>
-    <button on:click="{generate_context}">Generate a Verse</button>
-    <h2>{generate_contextMsg}</h2>
-  </div>
+<div class="container">
+
+    <button type="button" class="btn bg-gradient-to-br variant-gradient-warning-error" on:click="{generate_context}">Generate a verse</button>
+    <h3 class="h3"><span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">{generate_contextMsg}</span></h3>
+
+</div>
+
+<style>
+  .container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  }
+  h3 {
+  display: block;
+  margin: 0 auto;
+  height: 1.5rem;
+  text-align: center;
+  }
+</style> 
